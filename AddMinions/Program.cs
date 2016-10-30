07ce2467c0a6e47b6b287e6b10144346e0ed7a51
thrusanova <thrusanova@gmail.com>
2016-10-30 16:57:49 +0200

    add some task

---
+++ AddMinions/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddMinions
+{
+    class Program
+    {
+        public static SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=MinionsDB;Integrated Security=True");
+
+   
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            string minionName = input[1];
+            int age = int.Parse(input[2]);
+            string town = input[3];
+            input = Console.ReadLine().Split(' ');
+            string villiansName = input[1];
+            var minionTown = "SELECT Id FROM Towns WHERE name=@townName";
+            SqlCommand command = new SqlCommand(minionTown, connection);
+            command.Parameters.AddWithValue("@townName", town);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (!reader.HasRows)
+            {
+                reader.Close();
+                string townToAdd = "INSERT INTO Towns(name,country) VALUES(@townName,NULL)";
+                SqlCommand addTown = new SqlCommand(townToAdd, connection);
+                addTown.Parameters.AddWithValue("@townName", town);
+                addTown.ExecuteNonQuery();
+                Console.WriteLine("Town {0} was added to the database.",town);
+            }
+            reader.Close();
+            int townId = (int)command.ExecuteScalar();
+            reader.Close();
+
+            string villian = "SELECT * FROM Villains WHERE name=@villians";
+            SqlCommand cmd = new SqlCommand(villian, connection);
+            cmd.Parameters.AddWithValue("@villians", villiansName);
+            reader = cmd.ExecuteReader();
+            if (!reader.HasRows)
+            {
+                reader.Close();
+                string addVillainSQL = "INSERT INTO villains (name, EvilnessFactor) VALUES (@villians, 'evil')";
+                SqlCommand comd = new SqlCommand(addVillainSQL, connection);
+                comd.Parameters.AddWithValue("@villians",villiansName);
+                comd.ExecuteNonQuery();
+                Console.WriteLine("Villain {0} was added to the database.",villiansName);
+                   
+            }
+            reader.Close();
+            int villianId =(int) cmd.ExecuteScalar();
+            reader.Close();
+
+            string addMinion = "INSERT INTO Minions(Name,Age,TownId) VALUES(@name,@age,@townId)";
+            SqlCommand comm = new SqlCommand(addMinion, connection);
+            comm.Parameters.AddWithValue("@name", minionName);
+            comm.Parameters.AddWithValue("@age", age);
+            comm.Parameters.AddWithValue("@townId", townId);
+            comm.ExecuteNonQuery();
+            string getMinionId = "select id from Minions where name=@minionName";
+            cmd = new SqlCommand(getMinionId, connection);
+            cmd.Parameters.AddWithValue("@minionName", minionName);
+            int minionId = (int)cmd.ExecuteScalar();
+            string addMinionToVillian = "INSERT INTO MinionsVillains(MinionId,VillainId) VALUES(@minionId, @villainId)";
+            SqlCommand add = new SqlCommand(addMinionToVillian, connection);
+            add.Parameters.AddWithValue("@minionId", minionId);
+            add.Parameters.AddWithValue("@villainId", villianId);
+            add.ExecuteNonQuery();
+            Console.WriteLine("Successfully added {0} to be minion of {1}.",minionName,villiansName);
+        }
+    }
+}
---
+++ MinionNames/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MinionNames
+{
+    class Program
+    {
+        public static SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=MinionsDB;Integrated Security=True");
+
+        static void Main(string[] args)
+        {
+            var id = int.Parse(Console.ReadLine());
+            string viliansName = "SELECT Name FROM Villains WHERE id=@VillainId";
+            SqlCommand command = new SqlCommand(viliansName, connection);
+            command.Parameters.AddWithValue("@VillainId", id);
+            connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader(CommandBehavior.SingleResult))
+            {
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("There is no villiain with Id "+id +" in the database.");
+                    return;
+                }
+                reader.Read();
+                string name = reader[0].ToString();
+                Console.WriteLine("Name "+name);
+            }
+            connection.Close();
+            string minions="select m.name, age\n" +
+                    "FROM Villains v\n" +
+                    "JOIN MinionsVillains mv ON v.Id = mv.VillainId\n" +
+                    "JOIN Minions m ON m.id = mv.MinionId\n" +
+                    "WHERE v.Id = @villainId";
+            command = new SqlCommand(minions, connection);
+            command.Parameters.AddWithValue("@VillainId", id);
+            connection.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (!reader.HasRows)
+                {
+                    Console.WriteLine("No minions in the database");
+                    return;
+                }
+                int count = 1;
+                while (reader.Read())
+                {
+                    Console.WriteLine(count+" "+reader["name"]+" "+reader["age"]);
+                    count++;
+                }
+            }
+            connection.Close();
+         
+        }
+    }
+}
